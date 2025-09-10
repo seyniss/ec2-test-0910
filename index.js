20585c47329704e0ce6310cbfe53910cdb79b501
seyniss <shepunk2@kbu.ac.kr>
2025-09-10 19:27:58 +0900

    1

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express =require('express')
+const app =express()
+
+const PORT =process.env.PORT
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send(`.env 테스트:${process.env.DATABASE_NAME}`)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Sever is running! : ${PORT}`)
+})
